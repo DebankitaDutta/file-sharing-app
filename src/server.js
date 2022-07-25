68f407c36c9a699984008b29f63e2e8444b9969f
Vijay Palaskar <vijaypalaskar@rediffmail.com>
2022-07-25 21:47:56 +0530

    file upload issue fixed

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 const bp = require("body-parser");
 const cors = require('cors');
-const express=require('express')
+const express=require('express');
+const path = require('path');
 const app=express();
 const connectMDB= require('./config/db')
 const PORT= process.env.PORT ||4000
@@ -13,18 +14,22 @@ app.use(cors({
     methods: "GET, POST"
 }))
 
+app.use('/public', express.static(path.join(__dirname, '../assets')));
+
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 
 //apis
 app.use('/api/files',require('./routes/file'))
 app.use('/files',require('./routes/show'))
-
+app.get('/', (req, res ) => {
+    return res.sendFile(path.join(__dirname, './index.html'));
+});
 //setting up the view engine
 app.set('view engine','ejs')
 app.set('views','./views')
 
-connectMDB();
+// connectMDB();
 app.listen(PORT,()=>{
     console.log('server listens at ',PORT)
 })
