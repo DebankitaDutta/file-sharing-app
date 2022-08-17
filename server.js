a7bfd3ebe39010904ca62bea50f00e474c2eb8c0
debankita <sp@zuelligpharma.com>
2022-08-18 02:13:01 +0530

    took the server file out from the src folder

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const express=require('express');
 var favicon=require('serve-favicon')
 const path = require('path');
 const app=express();
-const connectMDB= require('./config/db')
+const connectMDB= require('./src/config/db')
 const PORT= process.env.PORT ||4000
 
-app.use(favicon(path.join(__dirname,'../assets/img','favicon.ico')))
+app.use(favicon(path.join(__dirname,'./assets/img','favicon.ico')))
 app.use(cors({
     origin:'*',
     optionsSuccessStatus: 200,
@@ -16,17 +16,17 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}));
-app.use('/public', express.static(path.join(__dirname, '../assets')));
+app.use('/public', express.static(path.join(__dirname, '/assets')));
 
 
 //apis
-app.use('/api/files',require('./routes/file'))
-app.use('/files',require('./routes/show'))
-app.use('/files/download',require('./routes/download'))
-app.use('/auth',require('./routes/auth')) // auth apis
+app.use('/api/files',require('./src/routes/file'))
+app.use('/files',require('./src/routes/show'))
+app.use('/files/download',require('./src/routes/download'))
+app.use('/auth',require('./src/routes/auth')) // auth apis
 
 app.get('/', (req, res ) => {
-    return res.sendFile(path.join(__dirname, './index.html'));
+    return res.sendFile(path.join(__dirname, './assets/index.html'));
 });
 
 //setting up the view engine
